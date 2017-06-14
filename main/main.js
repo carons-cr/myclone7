05c88ec1fecd3c5ad4be1a027d0de57badf70e30
carons-cr <2738794789@qq.com>
2017-06-14 19:25:11 +0800

    验证邮箱地址

--- main/main.js
+++ main/main.js
@@ -1,3 +1,17 @@
 module.exports = function main(email, suffixes) {
-  // Write your code here
+           var postfixArray = new Array();
+    	   postfixArray=suffixs;
+    	   var suffix;  	   
+    	   var index=email.lastIndexOf('@');
+    	   if(index!=-1){
+    		          suffix=email.substring(index+1);
+    	   }else{
+    		          return false;
+    	   }
+    	   for(var i=0;i<postfixArray.length;i++){
+	    		     if(postfixArray[i]==suffix){
+	    		    	 return true;
+	    		     }
+    	   }    
+    	   return false;
 };
